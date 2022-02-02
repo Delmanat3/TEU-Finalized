209f205fe9fd864035c068cfd4fecf07980b0463
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2022-02-02 15:11:25 -0600

    try 3

--- mongo/server.js
+++ mongo/server.js
@@ -24,16 +24,28 @@ server.applyMiddleware({ app });
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
   }
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+  let protected = ['app.css', 'favicon.ico']
 
+  app.get("*", (req, res) => {
+  
+	let path = req.params['0'].substring(1)
+  
+	if (protected.includes(path)) {
+	  // Return the actual file
+	  res.sendFile(`${__dirname}/build/${path}`);
+	} else {
+	  // Otherwise, redirect to /build/index.html
+	  res.sendFile(`${__dirname}/build/index.html`);
+	}
+  });
+  
 //* set up server to listen on port and open connection to graphql
 db.once('open', () => {
 	app.listen(PORT, () => {
