4741cd7458c80bb5aef834d504c097b8cbff2d7e
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2022-02-03 12:01:13 -0600

    stack has not been super helpful

--- mongo/server.js
+++ mongo/server.js
@@ -28,11 +28,11 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
   }
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '/client/public/index.html'));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 //* set up server to listen on port and open connection to graphql
